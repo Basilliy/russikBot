c4dc6066040088237328e52d32da75044610284c
russik2013 <z.kon2009@yandex.ru>
2016-07-12 17:36:11 +0300

    russ

--- index.php
+++ index.php
@@ -3,14 +3,14 @@
 //$update = file_get_contents($url."/getupdates");
 //$result = json_decode($_POST, TRUE);
 //$id = $_SERVER["result"][0]["message"]["chat"]["id"];
-
+$com = file_get_contents('php://input');
 $output = json_decode(file_get_contents('php://input'), TRUE);
 $chat_id = $output['message']['chat']['id'];
-$res = serialize(file_get_contents('php://input'));
+$res = serialize($com);
 print_r($res);
 //////////ВАЖНО /////
 $token = "246470400:AAElj-KNd6S9mTyo6wesYzyU8OrquBHQKRA";
-$url  = "https://api.telegram.org/bot".$token;
+$url  = "https://api.telegram.org/bot" . $token;
 $fuck = file_get_contents('https://evilinsult.com/generate_insult.php?lang=en');
 //echo $update;
 $update = file_get_contents($url."/sendmessage?chat_id='.$chat_id.'&text=".$fuck);
