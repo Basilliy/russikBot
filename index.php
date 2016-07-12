d7dcbe804b4c61dd9afe8165dfb9ce17f4b57ead
russik2013 <z.kon2009@yandex.ru>
2016-07-12 15:51:59 +0300

    russ

--- index.php
+++ index.php
@@ -3,10 +3,11 @@
 //$update = file_get_contents($url."/getupdates");
 //$result = json_decode($_POST, TRUE);
 //$id = $_SERVER["result"][0]["message"]["chat"]["id"];
-$output = json_decode(trim(file_get_contents('php://input')), TRUE);
+
+$output = json_decode(file_get_contents('php://input'), TRUE);
 $chat_id = $output['message']['chat']['id'];
-$message = $output['message']['text'];
-print_r($_POST['Update']);
+$first_name = $output['message']['chat']['first_name'];
+print_r('id='.$chat_id);
 
 //////////ВАЖНО /////
 $token = "246470400:AAElj-KNd6S9mTyo6wesYzyU8OrquBHQKRA";
