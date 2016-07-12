27616587b44b629f2ff60a5495eb1afc562512b6
russik2013 <z.kon2009@yandex.ru>
2016-07-13 00:16:00 +0300

    russ

--- index.php
+++ index.php
@@ -1,18 +1,20 @@
 <?php
-	function sendMessage($chat_id, $message)
-    {
-        file_get_contents($GLOBALS['api'] . '/sendMessage?chat_id=' . $chat_id . '&text=' . urlencode($message));
-    }
 
-	$access_token = '246470400:AAElj-KNd6S9mTyo6wesYzyU8OrquBHQKRA';
-	$api = 'https://api.telegram.org/bot' . $access_token;
-
-
-	$output = json_decode(file_get_contents('php://input'), TRUE);
-	$chat_id = $output['message']['chat']['id'];
-	$first_name = $output['message']['chat']['first_name'];
-	$message = $output['message']['text'];
-
-	$preload_text = $first_name . ', я получила ваше сообщение!';
-	sendMessage($chat_id, $preload_text);
-?>
+//$update = file_get_contents($url."/getupdates");
+//$result = json_decode($_POST, TRUE);
+//$id = $_SERVER["result"][0]["message"]["chat"]["id"];
+$com = file_get_contents('php://input');
+$output = json_decode(file_get_contents('php://input'), TRUE);
+$chat_id = $output['message']['chat']['id'];
+$res = serialize($_POST['Update']);
+$res1 = serialize($_GET);
+$res2 = serialize($com);
+print_r($res);
+print_r($res1);
+print_r($res2);
+//////////ВАЖНО /////
+$token = "246470400:AAElj-KNd6S9mTyo6wesYzyU8OrquBHQKRA";
+$url  = "https://api.telegram.org/bot" . $token;
+$fuck = file_get_contents('https://evilinsult.com/generate_insult.php?lang=en');
+//echo $update;
+$update = file_get_contents($url."/sendmessage?chat_id='.$chat_id.'&text=".$fuck);
