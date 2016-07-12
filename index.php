86eeda22fdb53ae1b7f6111bbbe02a1f4328060e
russik2013 <z.kon2009@yandex.ru>
2016-07-13 00:06:12 +0300

    russ

--- index.php
+++ index.php
@@ -1,29 +1,4 @@
 <?php
-function regHandler($token)
-{
-    $url = "https://api.telegram.org/bot" . $token . "/setWebhook";
-    $ch = curl_init();
-    $optArray = array(
-        CURLOPT_URL => $url,
-        CURLOPT_POST => true,
-        CURLOPT_SAFE_UPLOAD => false,
-        CURLOPT_RETURNTRANSFER => true,
-    );
-    curl_setopt_array($ch, $optArray);
-
-    $result = curl_exec($ch);
-    echo "<pre>";
-    print_r($result);
-    echo "</pre>";
-    curl_close($ch);
-}
-
-$token = '246470400:AAElj-KNd6S9mTyo6wesYzyU8OrquBHQKRA';
-$path = '/ssl/YOURPUBLIC.pem';
-$handlerurl = 'https://russikbot.herokuapp.com/index.php'; // ИЗМЕНИТЕ ССЫЛКУ
- 
-regHandler($token);
-
 	function sendMessage($chat_id, $message)
     {
         file_get_contents($GLOBALS['api'] . '/sendMessage?chat_id=' . $chat_id . '&text=' . urlencode($message));
