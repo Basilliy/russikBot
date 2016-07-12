3ce2fc817c11db6d8a9b4bbe625f5a7bd25a7b3a
russik2013 <z.kon2009@yandex.ru>
2016-07-12 15:15:04 +0300

    russ

--- index.php
+++ index.php
@@ -6,8 +6,8 @@
 $output = json_decode(file_get_contents('php://input'), TRUE);
 $chat_id = $output['message']['chat']['id'];
 $message = $output['message']['text'];
-print_r($_POST); 
-//echo curl_version(); 
+print_r($_POST);
+
 //////////ВАЖНО /////
 $token = "246470400:AAElj-KNd6S9mTyo6wesYzyU8OrquBHQKRA";
 $url  = "https://api.telegram.org/bot".$token;
