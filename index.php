f9c60700687f51565ec9fdd8f5e0dc5ecfc13081
russik2013 <z.kon2009@yandex.ru>
2016-07-13 15:45:31 +0300

    russ

--- index.php
+++ index.php
@@ -13,10 +13,10 @@ $message = $output['message']['text'];
 
 if($message == '/help'){
     $reply_markup = '';
-    $buttons = [[['text' => 'tekst',
-        'request_contact' => true ,
-        'request_location' => false ]]];
-     $buttons = ['refrefre' , 'erfre ' , 'erferf'];
+//    $buttons = [[['text' => 'tekst',
+//        'request_contact' => true ,
+//        'request_location' => false ]]];
+    // $buttons = ['refrefre' , 'erfre ' , 'erferf'];
     //'request_contact' => true]]];
     $buttons = [['10','23','23'],['10','23','23']];
     $keyboard = json_encode($keyboard = [
@@ -30,6 +30,7 @@ if($message == '/help'){
     $message = '121';
     sendMessage($chat_id, $message.$reply_markup);
 }
+
 else{
 
     $fuck = file_get_contents('https://evilinsult.com/generate_insult.php?lang=en');
