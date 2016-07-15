f211da56fe62b5843584e454a231d4aea7ef079f
Basilliy <z.kon2009@gmail.com>
2016-07-15 10:48:36 +0300

    Update index.php

--- index.php
+++ index.php
@@ -11,13 +11,13 @@ $chat_id = $output['message']['chat']['id'];
 $message = $output['message']['text'];
 $language = $output['callback_query']['data'];
 
-if($language =='/start')
+if($language =='en')
 {
     $message = "en";
     sendMessage($chat_id,$message);
 }
 
-if($language =='Hello, i am Marvin bot')
+if($language =='de')
 {
     $message = "de";
     sendMessage($chat_id,$message);
