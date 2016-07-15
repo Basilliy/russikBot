f9d0b6d22da1823782ecceb687a863fa967ca0dd
Basilliy <z.kon2009@gmail.com>
2016-07-15 13:30:54 +0300

    Update index.php

--- index.php
+++ index.php
@@ -165,12 +165,10 @@ function languageKeybord(){
 
 function inlineKeybord(){ ///Выводяться только с сообщением
 $reply_markup = '';
-$x1 = array('text' => 'open url',
-                'url' => 'https://github.com/akalongman/php-telegram-bot');
-$x2 = array('text' => 'open url',
-                'url' => 'https://github.com/akalongman/php-telegram-bot');
-$x3 = array("text"=>"ru","callback_data"=>"ru");
-$x4 = array("text"=>"fr","callback_data"=>"fr");
+$x1 = array('text'=>'en','callback_data'=>"en");
+$x2 = array('text'=>'de','callback_data'=>"de");
+$x3 = array('text'=>'ru','callback_data'=>"ru");
+$x4 = array('text'=>'fr','callback_data'=>"fr");
 $opz = [[$x1,$x2,$x3,$x4]];
 $keyboard=array("inline_keyboard"=>$opz);
 $keyboard = json_encode($keyboard);
