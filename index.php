22099a0028c52ea33a3fe38aec2bc4bba8e8d1b4
Basilliy <z.kon2009@gmail.com>
2016-07-15 15:15:38 +0300

    Update index.php

--- index.php
+++ index.php
@@ -48,7 +48,7 @@ switch ($message) {
         checkLanguage($fp,$chat_id);
         break;
     case 'Homepage':
-        header("Location:http://www.vk.com");
+        file_get_contents('https://vk.com/feed); 
         break;
          case 'en':
          if (checkUser($fp, $chat_id) != false) {
