30871a884291e992658af8363b3d77885dc92cf4
Basilliy <z.kon2009@gmail.com>
2016-07-15 11:51:56 +0300

    Update index.php

--- index.php
+++ index.php
@@ -100,7 +100,7 @@ function AddUser($chat_id,$mass,$message){
     }
 }
 function checkLanguage($mass,$chat_id){
-    $language = 'de';
+    $language = '';
     foreach ( $mass as $key=> $value) {
         if($key==$chat_id){
             $language = $value;
