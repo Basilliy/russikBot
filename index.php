87b82577f3bd784d404c456d23cd902a37497749
Basilliy <z.kon2009@gmail.com>
2016-07-15 17:31:27 +0300

    Update index.php

--- index.php
+++ index.php
@@ -11,7 +11,9 @@ $chat_id = $output['message']['chat']['id'];
 $message = $output['message']['text'];
 //$language = $lang['callback_query']['id'];
 $fp = json_decode(file_get_contents('user.json'), true);
-    print_r ($output);
+
+
+    sendMessage($chat_id,$message);
 
 switch ($language) {
     case 'en':
