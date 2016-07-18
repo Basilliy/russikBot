517c8aa0bb4670718bf014e3d3a7a0632cc89041
Basilliy <z.kon2009@gmail.com>
2016-07-18 16:42:34 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,14 @@
 <?php
    $access_token = '232279995:AAFMV0TUvJo9VAFvN-O9SRwDYozGRr9RjBM';
-   $url = 'https://api.telegram.org/bot' . $access_token;    $output = json_decode(file_get_contents('php://input'), true);
+   $url = 'https://api.telegram.org/bot' . $access_token;    
+   $output = json_decode(file_get_contents('php://input'), true);
    //$lang = json_decode(file_get_contents('php://input'), true);
    $chat_id = $output['message']['chat']['id'];
    $first_name = $output['message']['chat']['first_name'];
    $message = $output['message']['text'];  
-   $fp = json_decode(file_get_contents('user.json'), true);   
-   print_r('out=',serialize($output));$fp = json_decode(file_get_contents('user.json'), true);
+   //$fp = json_decode(file_get_contents('user.json'), true);   
+   //print_r('out=',serialize($output));
+   //$fp = json_decode(file_get_contents('user.json'), true);
    switch ($message) {
    case '/start':
    $text = 'russik';
@@ -38,7 +40,7 @@ $x2 = array('text'=>'de','callback_data'=>"de");
 $opz = [[$x1,$x2]];
 $keyboard=array("inline_keyboard"=>$opz);
 $keyboard = json_encode($keyboard,true);
-$reply_markup = '&reply_markup=' . $keyboard . '';   
+$reply_markup = '&reply_markup=' . $keyboard;   
 return $reply_markup;
 }
 
