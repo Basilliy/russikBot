59a80e62e8484cb99be6af5afe747f9ae4ce28dc
Basilliy <z.kon2009@gmail.com>
2016-07-20 19:06:53 +0300

    Update index.php

--- index.php
+++ index.php
@@ -195,8 +195,8 @@ function HTML(){
     
 }
 function forURL(){
-    $HTML = '<b>bold</b>, <strong>bold</strong>
-<i>italic</i>, <em>italic</em>
-<a href="https://evilinsult.com/">inline URL</a>';
+    $HTML = '<b></b>, <strong></strong>
+<i></i>, <em></em>
+<a href="https://evilinsult.com/">"Evil Insult Generator Homepage</a>';
     return $HTML;
 }
