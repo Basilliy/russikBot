69b402a7c1dc6ba2c40bde4770591faa73f72b1a
Basilliy <z.kon2009@gmail.com>
2016-07-20 18:50:25 +0300

    Update index.php

--- index.php
+++ index.php
@@ -188,9 +188,7 @@ return $file_array[28];
 function HTML(){
     $HTML = '<b>bold</b>, <strong>bold</strong>
     <i>italic</i>, <em>italic</em>
-    <a href="https://habrahabr.ru/post/221949/">site</a>
-    <code>inline fixed-width code</code>
-    <pre>pre-formatted fixed-width code block</pre>';
+    <a href="https://evilinsult.com/">Evil Insult Generator Homepage</a>';
     return $HTML;
     
 }
