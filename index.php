b8208da313cbf599f31cf1e7129490b3e2c1df88
Basilliy <z.kon2009@gmail.com>
2016-07-25 15:40:11 +0300

    Update index.php

--- index.php
+++ index.php
@@ -8,13 +8,9 @@ $output = json_decode(file_get_contents('php://input'), true);
 $chat_id = $output['message']['chat']['id'];
 $message = $output['message']['text'];
 $fp = json_decode(file_get_contents('user.json'), true);
-if(isset($output['inline_query']['from']['id'])){
-   $chat_id = $output['message']['chat']['id'];
-   $message = $output['message']['text'];
-  file_get_contents("https://api.telegram.org/bot246470400:AAElj-KNd6S9mTyo6wesYzyU8OrquBHQKRA/sendMessage?chat_id=".$chat_id."&text=".$message."&parse_mode=HTML");         
-  
-}
 
+ file_get_contents("https://api.telegram.org/bot246470400:AAElj-KNd6S9mTyo6wesYzyU8OrquBHQKRA/sendMessage?chat_id=".$chat_id."&text=".$message."&parse_mode=HTML");         
+  
 if(isset($output['callback_query']['data'])){
 
 
